45ccd5e7d033e6d51320f02085936520fbe327ec
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-03-26 21:41:52 +0530

    imports in python

---
+++ fast-api/eric-rody/pythonProjects/pythonRefresher/refresher-code/Imports/homework.grades.py
@@ -0,0 +1,10 @@
+import grade_average_service as grade_service
+
+homework_assignment_grades={
+    'homework_1':90,
+    'homework_2':100,
+    'homework_3':81
+}
+
+grade_service.calculate_homework(homework_assignment_grades)
+
