cba9fdd61e3d3c2a10f4ead18fe6355af1ad1ecc
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-03-28 22:58:05 +0530

    fastapi - initial commit

--- fast-api/eric-rody/pythonProjects/fastapi/fastapienv/books.py
+++ fast-api/eric-rody/pythonProjects/fastapi/fastapienv/books.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
 app=FastAPI()
+# run the app -  uvicorn books:app --reload
 
-#  uvicorn books:app --reload
-@app.get("/api-endpoint")
-async def first_api():
-    return {"message":"Hello Nithin"}
+BOOKS=[
+    {'title': 'Title One', 'author': "Author One", "category":"science"},
+    {'title': 'Title Two', 'author': "Author Two", "category": "science"},
+    {'title': 'Title Three', 'author': "Author Three", "category": "history"},
+    {'title': 'Title Four', 'author': "Author Four", "category": "math"},
+    {'title': 'Title Five', 'author': "Author Five", "category": "math"},
+    {'title': 'Title Six', 'author': "Author Six", "category": "math"},
+]
+# if api endpoint is repeated make sure the dynamic param is below the static param or the static value will never get executed
+@app.get("/books/mybook")
+async def read_all_books():
+    return {'book_title':'My favourite book!'}
+
+# Dynamic Param
+@app.get("/books/{book_title}")
+# the dynamic param will be changed to a string eg number 1 becomes string "1"
+async def read_all_books(book_title:str):
+    for book in BOOKS:
+        # casefold makes string lowercase
+        if book.get('title').casefold()==book_title.casefold():
+            return book
+#  Query Parameters
