425248f94131f4d22e38066a780414fd8a168974
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-03-29 09:24:15 +0530

    added post request

--- fast-api/eric-rody/pythonProjects/fastapi/fastapienv/books.py
+++ fast-api/eric-rody/pythonProjects/fastapi/fastapienv/books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 app=FastAPI()
 # run the app -  uvicorn books:app --reload
 
@@ -12,14 +12,19 @@ BOOKS=[
 ]
 # if api endpoint is repeated make sure the dynamic param is below the static param or the static value will never get executed
 
+# read all books
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
+
 # Dynamic Param
-# @app.get("/books/{book_title}")
-# # the dynamic param will be changed to a string eg number 1 becomes string "1"
-# async def read_all_books(book_title:str):
-#     for book in BOOKS:
-#         # casefold makes string lowercase
-#         if book.get('title').casefold()==book_title.casefold():
-#             return book
+@app.get("/books/{book_title}")
+# the dynamic param will be changed to a string eg number 1 becomes string "1"
+async def read_book(book_title:str):
+    for book in BOOKS:
+        # casefold makes string lowercase
+        if book.get('title').casefold()==book_title.casefold():
+            return book
 # search book by category
 @app.get("/books/")
 async def read_category_by_query(category:str):
@@ -41,3 +46,9 @@ async def read_author_category_by_query(book_author:str,category:str):
             book.get('category').casefold()==category.casefold():
             books_to_return.append(book)
         return books_to_return
+
+
+# to use body for post request we need to import BODY
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
