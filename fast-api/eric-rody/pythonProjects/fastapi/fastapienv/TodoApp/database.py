605ce6eb72725a6a77f724e96ecf938bdd8aec5b
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-04-08 20:54:06 +0530

    added postgresql connectionstring

--- fast-api/eric-rody/pythonProjects/fastapi/fastapienv/TodoApp/database.py
+++ fast-api/eric-rody/pythonProjects/fastapi/fastapienv/TodoApp/database.py
@@ -2,11 +2,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL="sqlite:///./todos.db"
-# db engine
+# sqllite connection
+# SQLALCHEMY_DATABASE_URL="sqlite:///./todos.db"
+# db engine-sqlite
+# engine=create_engine(
+#     SQLALCHEMY_DATABASE_URL,connect_args={"check_same_thread":False}
+# )
+
+# postgrsql connection
+SQLALCHEMY_DATABASE_URL="postgresql://postgres:admin@localhost/TodoApplicationDatabase"
 engine=create_engine(
-    SQLALCHEMY_DATABASE_URL,connect_args={"check_same_thread":False}
+    SQLALCHEMY_DATABASE_URL
 )
+
 # db session - create an instance of a session
 SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 # create db model
