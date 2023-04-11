c3501415b9716be51945f9645aa182840bb42595
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-04-11 21:51:08 +0530

    added users api routes

--- fast-api/eric-rody/pythonProjects/fastapi/fastapienv/TodoApp/main.py
+++ fast-api/eric-rody/pythonProjects/fastapi/fastapienv/TodoApp/main.py
@@ -3,17 +3,18 @@ from fastapi import FastAPI,Depends
 import models
 from database import engine,SessionLocal
 # import router file
-from routers import auth,todos
+from routers import auth,todos,users
 # simulate importing of another api
 from company import companyapis,dependencies
 
 app=FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
-# include the auth file
+# include the files using router
 app.include_router(auth.router)
-# include the router in todos
 app.include_router(todos.router)
+app.include_router(users.router)
+
 # include the router in company apis
 app.include_router(
     companyapis.router,
