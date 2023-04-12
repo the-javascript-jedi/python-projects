73df9d4f27bf31d46bbfcf69927bbb0289b391cc
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-04-12 17:54:58 +0530

    added script for alembic add new column

--- fast-api/eric-rody/pythonProjects/fastapi/fastapienv/database.py
+++ fast-api/eric-rody/pythonProjects/fastapi/fastapienv/database.py
@@ -10,22 +10,24 @@ from sqlalchemy.ext.declarative import declarative_base
 # )
 
 # postgrsql connection
-# SQLALCHEMY_DATABASE_URL="postgresql://postgres:admin@localhost/TodoApplicationDatabase"
-# engine=create_engine(
-#     SQLALCHEMY_DATABASE_URL
-# )
-#
-# # db session - create an instance of a session
-# SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
-
-# mysql connection
-SQLALCHEMY_DATABASE_URL="mysql+pymysql://root:mysqlroot@127.0.0.1:3306/todoapp"
+SQLALCHEMY_DATABASE_URL="postgresql://postgres:admin@localhost/TodoApplicationDatabase"
 engine=create_engine(
     SQLALCHEMY_DATABASE_URL
 )
 
 # db session - create an instance of a session
 SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
+
+# # mysql connection
+# SQLALCHEMY_DATABASE_URL="mysql+pymysql://root:mysqlroot@127.0.0.1:3306/todoapp"
+# engine=create_engine(
+#     SQLALCHEMY_DATABASE_URL
+# )
+#
+# # db session - create an instance of a session
+# SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
+#
+
 
 # create db model
 Base=declarative_base()
