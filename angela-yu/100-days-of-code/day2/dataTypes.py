c42f861a2a76e57f68f1bf9387cc090d429d2c63
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-05-20 00:09:22 +0530

    day 2 code

---
+++ angela-yu/100-days-of-code/day2/dataTypes.py
@@ -0,0 +1,9 @@
+print('hello world')
+two_digit_number=input("Type a 2 digit number")
+print(type(two_digit_number))
+print("type(two_digit_number)",type(two_digit_number))
+first_digit=two_digit_number[0]
+second_digit=two_digit_number[1]
+
+result=int(first_digit)+int(second_digit)
+print("result",result)
