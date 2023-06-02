4d28182f371c33788173bce009caefaf7099ff0f
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-06-02 20:50:05 +0530

    checked in get section

--- fast-api/catalin-stefan/fastapi-practice/main.py
+++ fast-api/catalin-stefan/fastapi-practice/main.py
@@ -1,7 +1,15 @@
+from typing import Optional
 from fastapi import FastAPI
 
-app=FastAPI()
+app = FastAPI()
 
-@app.get("/")
-def index():
-    return "Hello World"
+# @app.get('/blog/all')
+# # the request should contain the same name (i.e page,page_size)
+# # http://127.0.0.1:8000/blog/all?page=1&page_size=11
+# def get_blogs(page = 1, page_size: Optional[int] = None):
+#   return {'message': f'All {page_size} blogs on page {page}'}
+
+# combination of query params and path params
+@app.get('blog/{id}/comments/{comment_id}')
+def get_comment(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
+  return {'message': f'blog_id {id}, comment_id {comment_id}, valid {valid}, username {username}'}
