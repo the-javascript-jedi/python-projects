916bb86c25f10e9f0e584f08c3c8144cc56650e0
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-06-11 00:02:35 +0530

    added router for apis

--- fast-api/catalin-stefan/fastapi-practice/main.py
+++ fast-api/catalin-stefan/fastapi-practice/main.py
@@ -1,16 +1,12 @@
 from fastapi import FastAPI
-
+from router import blog_get
+from router import blog_post
 
 app = FastAPI()
+app.include_router(blog_get.router)
+app.include_router(blog_post.router)
+
 
 @app.get('/hello')
 def index():
   return {'message': 'Hello world!'}
-
-@app.get('/blog/all')
-def get_all_blogs():
-  return {'message': 'All blogs provided'}
-
-@app.get('/blog/{id}')
-def get_blog(id: int):
-  return {'message': f'Blog with id {id}'}
