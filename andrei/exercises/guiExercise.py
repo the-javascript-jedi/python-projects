f1bc05c4c731a56fb3188f32155ef3a643830abc
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-06-10 22:12:45 +0530

    gui exercise added

---
+++ andrei/exercises/guiExercise.py
@@ -0,0 +1,24 @@
+# print a christmas tree
+picture = [
+[0,0,0,1,0,0,0],
+[0,0,1,1,1,0,0],
+[0,1,1,1,1,1,0],
+[1,1,1,1,1,1,1],
+[0,0,0,1,0,0,0],
+[0,0,0,1,0,0,0]
+]
+for row in picture:
+    for pixel in row:
+        if (pixel == 1):
+            print('*', end = '')
+        else:
+            print(' ', end = '')
+    print('')
+
+# o/p
+#    *
+#   ***
+#  *****
+# *******
+#    *
+#    *
