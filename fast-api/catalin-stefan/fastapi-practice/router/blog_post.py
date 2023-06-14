775f98b657a3a989d7b400e77baba5b1e34e6a1c
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-06-14 20:38:43 +0530

    added post apis with list params in path

--- fast-api/catalin-stefan/fastapi-practice/router/blog_post.py
+++ fast-api/catalin-stefan/fastapi-practice/router/blog_post.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional,List,Dict
 from fastapi import APIRouter, Query, Body
 from pydantic import BaseModel
 
@@ -6,12 +6,19 @@ router = APIRouter(
     prefix='/blog',
     tags=['blog']
 )
+# Image class to be used as a type in BlogModel class
+class Image(BaseModel):
+    url:str
+    alias:str
 
 class BlogModel(BaseModel):
   title: str
   content: str
   nb_comments: int
   published: Optional[bool]
+  tags:List[str]=[]
+  metadata:Dict[str,str]={'key1':'val1'}
+  image:Optional[Image]=None
 
 @router.post('/new/{id}')
 def create_blog(blog: BaseModel, id: int, version: int = 1):
@@ -34,11 +41,13 @@ def create_comment(blog: BlogModel, id: int,
             max_length=50,
             # Lowercase or spaces
             regex='^[a-z\s]*$'
-        )
+        ),
+        v:Optional[List[str]]=Query(None)
     ):
     return {
         'blog': blog,
         'id': id,
         'comment_id': comment_id,
-        'content': content
+        'content': content,
+        'version':v
     }
