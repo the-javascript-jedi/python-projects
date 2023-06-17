6027c0651261fca753bff74a5ae930c7d302445c
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-06-17 13:58:17 +0530

    advanced functions

---
+++ andrei/exercises/functionalProgarms.py
@@ -0,0 +1,29 @@
+# map example
+# my_list=[1,2,3]
+# def multiply_by2(item):
+#     return item*2
+# print(list(map(multiply_by2,my_list)))
+# print(my_list)
+# filter
+# my_list=[1,2,3]
+#
+# def only_odd(item):
+#     return item%2!=0
+#
+# print("list(filter(only_odd,my_list))",list(filter(only_odd,my_list)))
+# print("my_list",my_list)
+
+# zip
+# my_list = [1, 2, 3]
+# your_list=[10,20,30]
+# print(list(zip(my_list,your_list)))
+
+
+# reduce
+from functools import reduce
+my_list=[1,2,3]
+def accumulator(acc,item):
+    print(acc,item)
+    return acc+item
+
+print(reduce(accumulator,my_list,0))
