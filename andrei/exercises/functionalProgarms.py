e9c079fe223106562265d2bedafbcbf4ab9f381e
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-06-17 14:13:51 +0530

    added labda functions

--- andrei/exercises/functionalProgarms.py
+++ andrei/exercises/functionalProgarms.py
@@ -20,10 +20,30 @@
 
 
 # reduce
-from functools import reduce
-my_list=[1,2,3]
-def accumulator(acc,item):
-    print(acc,item)
-    return acc+item
+# from functools import reduce
+# my_list=[1,2,3]
+# def accumulator(acc,item):
+#     print(acc,item)
+#     return acc+item
+#
+# print(reduce(accumulator,my_list,0))
 
-print(reduce(accumulator,my_list,0))
+# # lambda expressions - map
+# my_list=[1,2,3]
+# print(list(map(lambda item:item*2,my_list)))
+
+# # lambda expressions - filter
+my_list=[1,2,3]
+print(list(filter(lambda item:item%2,my_list)))
+print(my_list)
+
+# # lambda expressions - reduce
+# from functools import reduce
+# my_list=[1,2,3]
+# def accumulator(acc,item):
+#     print(acc,item)
+#     return acc+item
+#
+# print(reduce(lambda acc,item:acc+item,my_list))
+# print(my_list)
+
