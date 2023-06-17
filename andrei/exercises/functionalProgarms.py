3f59da38c1e9608339e504a77a99fc019d612734
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-06-17 19:40:45 +0530

    lambda expression exercise

--- andrei/exercises/functionalProgarms.py
+++ andrei/exercises/functionalProgarms.py
@@ -33,9 +33,9 @@
 # print(list(map(lambda item:item*2,my_list)))
 
 # # lambda expressions - filter
-my_list=[1,2,3]
-print(list(filter(lambda item:item%2,my_list)))
-print(my_list)
+# my_list=[1,2,3]
+# print(list(filter(lambda item:item%2,my_list)))
+# print(my_list)
 
 # # lambda expressions - reduce
 # from functools import reduce
@@ -47,3 +47,20 @@ print(my_list)
 # print(reduce(lambda acc,item:acc+item,my_list))
 # print(my_list)
 
+# Lambda Expression - Exercise
+# Exercise 1 -  square the list
+my_list=[5,4,3]
+new_list=list(map(lambda item:item**2,my_list))
+print("new_list",new_list)
+
+# Exercise 2 - sort the list based on second value of tuple
+a=[(0,2),(4,3),(10,-1),(9,9),]
+print("unsorted",a)
+# sort by the first value of tuple
+a.sort()
+print("sorted",a)
+# for custom sort provide a key
+# using lambda function we need to get the first key sorted
+sortedList = a.copy()
+sortedList.sort(key=lambda x:x[1])
+print("sortedList",sortedList)
