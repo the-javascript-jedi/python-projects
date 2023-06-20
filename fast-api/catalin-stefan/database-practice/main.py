35421f7ebf415646e83511f4b2730b67c1e4a1f2
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-06-20 23:43:27 +0530

    added headers

--- fast-api/catalin-stefan/database-practice/main.py
+++ fast-api/catalin-stefan/database-practice/main.py
@@ -7,6 +7,7 @@ from router import blog_get
 from router import blog_post
 from router import user
 from router import article
+from router import product
 from db import models
 # import the engine from the created db file
 from db.database import engine
@@ -19,6 +20,7 @@ app.include_router(user.router)
 app.include_router(article.router)
 app.include_router(blog_get.router)
 app.include_router(blog_post.router)
+app.include_router(product.router)
 
 @app.get("/hello")
 def index():
