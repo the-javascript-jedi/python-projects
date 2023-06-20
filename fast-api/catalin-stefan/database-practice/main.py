d29124f1a09cbc461194544559454ca45f6051ad
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-06-20 20:38:23 +0530

    created custom exceptions

--- fast-api/catalin-stefan/database-practice/main.py
+++ fast-api/catalin-stefan/database-practice/main.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI
+from email.policy import HTTP
+
+from fastapi import FastAPI, HTTPException
+
+from exceptions import StoryException
 from router import blog_get
 from router import blog_post
 from router import user
@@ -6,6 +10,9 @@ from router import article
 from db import models
 # import the engine from the created db file
 from db.database import engine
+from fastapi import Request
+from fastapi.responses import JSONResponse
+from fastapi.responses import PlainTextResponse
 
 app=FastAPI()
 app.include_router(user.router)
@@ -16,5 +23,16 @@ app.include_router(blog_post.router)
 @app.get("/hello")
 def index():
     return {"message":"Hello World!"}
+
+@app.exception_handler(StoryException)
+def story_exception_handler(request:Request,exc:StoryException):
+    return JSONResponse(
+        status_code=418,
+        content={"detail":exc.name}
+    )
+# this will handle all exceptions globally, so better to comment it out so it wont interfere with future lectures
+@app.exception_handler(HTTPException)
+def custom_handler(request:Request,exc:StoryException):
+    return PlainTextResponse(str(exc),status_code=400)
 
 models.Base.metadata.create_all(engine)
