acdc8932ca106319bed376353d17cbb08fc00073
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-06-21 23:46:29 +0530

    added generating access token for oauth

--- fast-api/catalin-stefan/database-practice/main.py
+++ fast-api/catalin-stefan/database-practice/main.py
@@ -1,53 +1,47 @@
-from fastapi import FastAPI, HTTPException
-
-from exceptions import StoryException
-from router import blog_get
-from router import blog_post
-from router import user
-from router import article
-from router import product
-from db import models
-# import the engine from the created db file
+from typing import Optional
+from fastapi import FastAPI, Request
+from router import blog_get, blog_post, user, article, product
+from auth import authentication
 from db.database import engine
-from fastapi import Request
+from db import models
+from exceptions import StoryException
 from fastapi.responses import JSONResponse
-from fastapi.responses import PlainTextResponse
 from fastapi.middleware.cors import CORSMiddleware
 
-app=FastAPI()
+
+app = FastAPI()
+app.include_router(authentication.router)
 app.include_router(user.router)
 app.include_router(article.router)
+app.include_router(product.router)
 app.include_router(blog_get.router)
 app.include_router(blog_post.router)
-app.include_router(product.router)
 
-@app.get("/hello")
+@app.get('/hello')
 def index():
-    return {"message":"Hello World!"}
+  return {'message': 'Hello world!'}
 
 @app.exception_handler(StoryException)
-def story_exception_handler(request:Request,exc:StoryException):
-    return JSONResponse(
-        status_code=418,
-        content={"detail":exc.name}
-    )
-# this will handle all exceptions globally, so better to comment it out so it wont interfere with future lectures
-@app.exception_handler(HTTPException)
-def custom_handler(request:Request,exc:StoryException):
-    return PlainTextResponse(str(exc),status_code=400)
+def story_exception_handler(request: Request, exc: StoryException):
+  return JSONResponse(
+    status_code=418,
+    content={'detail': exc.name}
+  )
+
+# @app.exception_handler(HTTPException)
+# def custom_handler(request: Request, exc: StoryException):
+#   return PlainTextResponse(str(exc), status_code=400)
 
 models.Base.metadata.create_all(engine)
 
-# origins paths
-origins=[
-    "http://localhost:3000"
+origins = [
+  'http://localhost:3000'
 ]
 
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=["*"],
-    # allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
+  CORSMiddleware,
+  allow_origins = origins,
+  allow_credentials = True,
+  allow_methods = ["*"],
+  allow_headers = ['*']
 )
