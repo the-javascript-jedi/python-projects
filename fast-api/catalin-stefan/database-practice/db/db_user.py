708c596dfee908ea288216e8ec8098f506625a13
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-06-22 00:58:43 +0530

    added authentication code

--- fast-api/catalin-stefan/database-practice/db/db_user.py
+++ fast-api/catalin-stefan/database-practice/db/db_user.py
@@ -25,6 +25,13 @@ def get_user(db: Session, id: int):
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'User with id {id} not found')
   return user
 
+def get_user_by_username(db: Session, username: str):
+  user =db.query(DbUser).filter(DbUser.username == username).first()
+  # Handle errors
+  if not user:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'User with username {username} not found')
+  return user
+
 def update_user(db: Session, id: int, request: UserBase):
   user =  db.query(DbUser).filter(DbUser.id == id)
   if not user:
--- fast-api/catalin-stefan/database-practice/router/article.py
+++ fast-api/catalin-stefan/database-practice/router/article.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from db.database import get_db
 from db import db_article
-from auth.oauth2 import oauth2_scheme
+from auth.oauth2 import oauth2_scheme,get_current_user
 
 router = APIRouter(
   prefix='/article',
@@ -13,12 +13,16 @@ router = APIRouter(
 
 # Create article
 @router.post('/', response_model=ArticleDisplay)
-def create_article(request: ArticleBase, db: Session = Depends(get_db)):
+def create_article(request: ArticleBase, db: Session = Depends(get_db),current_user:UserBase = Depends(get_current_user)):
   return db_article.create_article(db, request)
 
 # Get specific article
 #  we are securing this api through a token
 
-@router.get('/{id}', response_model=ArticleDisplay)
-def get_article(id: int, db: Session = Depends(get_db),token:str = Depends(oauth2_scheme)):
-  return db_article.get_article(db, id)
+# @router.get('/{id}', response_model=ArticleDisplay)
+@router.get('/{id}')
+def get_article(id: int, db: Session = Depends(get_db),current_user:UserBase = Depends(get_current_user)):
+  return{
+    'data':db_article.get_article(db, id),
+    'current_year':current_user
+  }
