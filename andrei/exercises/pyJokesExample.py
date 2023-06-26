d7eab5d89471a6bdbde76d518da9859a19880128
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-06-26 22:18:36 +0530

    added modules section

---
+++ andrei/exercises/pyJokesExample.py
@@ -0,0 +1,5 @@
+import pyjokes
+# english, neutral geek jokes
+joke=pyjokes.get_joke('en','neutral')
+
+print("joke",joke)
---
+++ andrei/exercises/usefulModules.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+# Counter example in list
+li=[1,2,3,4,5,6,7,7]
+print("Counter(li)",Counter(li))
+
+# Counter example in string
+sentence = "blah blah blah thinking about python"
+print("Counter(sentence)",Counter(sentence))
