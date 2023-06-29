c5ff3f30ba5f6ed68b4ac821262bc4c1f1f506e7
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-06-29 22:11:57 +0530

    added Jpgtopng converter code

---
+++ andrei/image-playground/JpgToPngConverter.py
@@ -0,0 +1,21 @@
+# to run program - python JpgToPngConverter.py Pokedex / new /
+import sys
+import os
+from PIL import Image
+
+# grab first and second argument
+image_folder=sys.argv[1]
+output_folder=sys.argv[2]
+
+print(image_folder,output_folder)
+# check if new folder exists, if not create the folder
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+# loop through entire folder and convert the images to png
+for filename in os.listdir(image_folder):
+    img=Image.open(f'{image_folder}{filename}')
+    # remove existing .jpg in filename and append png
+    clean_name=os.path.splitext(filename)[0]
+    img.save(f'{output_folder}{clean_name}.png','png')
+    print('all done')
