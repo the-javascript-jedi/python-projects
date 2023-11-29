f8fef22adf32ae567f25e4e5cb51865a854cf06d
the-javascript-jedi <nithin.thewebdev@gmail.com>
2023-11-29 15:57:40 +0530

    added streamlit and added one page

---
+++ ardit-sulche/build-20-apps/app-2-portfolio/main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1,col2 =st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("the-javascript-jedi")
+    content="""
+    Hi I am the-javascript-jedi and I am also Batman learning python and docker I am a front end developer wanting to transition to backend
+    """
+    st.info(content)
