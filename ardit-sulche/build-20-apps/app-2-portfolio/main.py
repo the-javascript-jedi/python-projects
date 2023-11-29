ce3b351a03eead94bdcba1b78574d633307acd43
the-javascript-jedi <nithin.thewebdev@gmail.com>
2023-11-29 21:04:26 +0530

    added description

--- ardit-sulche/build-20-apps/app-2-portfolio/main.py
+++ ardit-sulche/build-20-apps/app-2-portfolio/main.py
@@ -2,14 +2,16 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
-col1,col2 =st.columns(2)
+col1, col2 = st.columns(2)
 
 with col1:
     st.image("images/photo.png")
 
 with col2:
-    st.title("the-javascript-jedi")
-    content="""
-    Hi I am the-javascript-jedi and I am also Batman learning python and docker I am a front end developer wanting to transition to backend
+    st.title("Ardit Sulce")
+    content = """
+    Hi, I am the javascript jedi! I am a Python programmer.
     """
     st.info(content)
+content2="""Below you can find some of the apps I have built in python, please feel free to contact me"""
+st.write(content2)
