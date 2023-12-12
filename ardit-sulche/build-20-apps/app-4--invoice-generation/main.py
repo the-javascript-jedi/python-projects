179797d44a07175059f6c076aa6ae655018e16b6
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-12-12 22:36:22 +0530

    invoice generator added pdf generation

--- ardit-sulche/build-20-apps/app-4--invoice-generation/main.py
+++ ardit-sulche/build-20-apps/app-4--invoice-generation/main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import glob
+from fpdf import FPDF
+from pathlib import Path
 
 # take all files inside invoices with the extension .xlsx
 # glob.glob will return an array of file names
@@ -9,4 +11,12 @@ print(filepaths)
 for filepath in filepaths:
     # loop 3 excels and create data frames for each
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
-    print(df)
+    pdf = FPDF(orientation="P",unit="mm",format="A4")
+    pdf.add_page()
+    # excel name - 10001-2023.1.18.xlsx
+    # using Path we can form an intelligent string, in this string .stem
+    filename=Path(filepath).stem
+    invoice_nr=filename.split("-")[0]
+    pdf.set_font(family="Times",size=16,style="B")
+    pdf.cell(w=50,h=8,txt=f"Invoice nr.{invoice_nr}")
+    pdf.output(f"PDFs/{filename}.pdf")
