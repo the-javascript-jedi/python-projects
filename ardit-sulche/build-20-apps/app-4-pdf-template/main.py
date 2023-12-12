bee581b4fd9cfa6f27639ded712fdd97ba9e6478
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-12-12 19:30:57 +0530

    generated pdf from csv

--- ardit-sulche/build-20-apps/app-4-pdf-template/main.py
+++ ardit-sulche/build-20-apps/app-4-pdf-template/main.py
@@ -2,6 +2,8 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation="P",unit="mm",format="A4")
+# remove automatic page break
+pdf.set_auto_page_break(auto=False,margin=0)
 
 df=pd.read_csv("topics.csv")
 
@@ -10,8 +12,32 @@ for index,row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=12)
     pdf.set_text_color(0,0,0)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    #line coordinates -- pdf.line(x1,y1,x2,y2)
-    pdf.line(10,21,200,21)
+    # draw blank lines  #line coordinates -- pdf.line(x1,y1,x2,y2)
+    # start at 20 till 298 with 10 distance between each line
+    for y in range(20,298,10):
+        pdf.line(10,y,200,y)
+    # add footer for all pages
+    # set position of footer
+    pdf.ln(265)
+    pdf.set_font(family="Times",style="I",size=8)
+    pdf.set_text_color(0, 0, 0)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="R")
+
+    # each topic has a set of pages - use nested for loop
+    for i in range(row["Pages"]-1):
+        pdf.add_page()
+        # add footer for all pages
+        # set position of footer
+        pdf.ln(277)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(0, 0, 0)
+        pdf.cell(w=0, h=12, txt=row["Topic"], align="R")
+        for i in range(10):
+            pdf.set_draw_color(0, 0, 0)
+            pdf.cell(w=0, h=12, txt="Hello", align="R",ln=1,border=1)
+            pdf.line(10, 21, 200, 21)
+            for y in range(20, 298, 10):
+                pdf.line(10, y, 200, y)
 
 # output.pdf is generated in the file folder
 pdf.output("output.pdf")
