2c96d034f3deaa7a813202caf6d21139006b766b
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-12-12 21:57:15 +0530

    invoice generator added pandas to read excel

--- ardit-sulche/build-20-apps/app-4--invoice-generation/main.py
+++ ardit-sulche/build-20-apps/app-4--invoice-generation/main.py
@@ -5,3 +5,8 @@ import glob
 # glob.glob will return an array of file names
 filepaths = glob.glob("invoices/*.xlsx")
 print(filepaths)
+
+for filepath in filepaths:
+    # loop 3 excels and create data frames for each
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    print(df)
