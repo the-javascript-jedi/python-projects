f206b05c6b4743c9ddc183f10b7759eb38dc61eb
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-12-12 11:37:07 +0530

    generated pdf from csv

--- ardit-sulche/build-20-apps/app-4-pdf-template/main.py
+++ ardit-sulche/build-20-apps/app-4-pdf-template/main.py
@@ -1,18 +1,17 @@
 from fpdf import FPDF
+import pandas as pd
 
 pdf = FPDF(orientation="P",unit="mm",format="A4")
 
-# print(type(pdf))
-pdf.add_page()
+df=pd.read_csv("topics.csv")
 
-pdf.set_font(family="Times",style="B",size=12)
-# w=0 - entire length of box
-# ln=1 - break line add to next line
-# h=12 - try to match font size with height
-# align="L" - align left to right
-pdf.cell(w=0,h=12,txt="Hello There!",align="L",ln=1,border=1)
-# Ctrl + D copys the line
-pdf.set_font(family="Times",style="B",size=9)
-pdf.cell(w=0,h=12,txt="Hi There!",align="L",ln=1,border=1)
+for index,row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font(family="Times", style="B", size=12)
+    pdf.set_text_color(0,0,0)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+    #line coordinates -- pdf.line(x1,y1,x2,y2)
+    pdf.line(10,21,200,21)
+
 # output.pdf is generated in the file folder
 pdf.output("output.pdf")
