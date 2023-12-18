8bc8e600992729b08bb25432550d9ba788fb18c7
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-12-18 23:43:22 +0530

    added api basic query params

--- ardit-sulche/build-20-apps/app-6--your-weather-api/main.py
+++ ardit-sulche/build-20-apps/app-6--your-weather-api/main.py
@@ -1,13 +1,19 @@
 from flask import Flask,render_template
 # name of object - can be anything
-app=Flask("Website")
+# __name__ -
+app=Flask(__name__)
 
-@app.route("/home")
+@app.route("/")
 def home():
     return render_template("tutorial.html")
 
-@app.route("/about/")
-def about():
-    return render_template("about.html")
-
-app.run(debug=True)
+@app.route("/api/v1/<station>/<date>")
+def about(station,date):
+    temperature=23
+    return {
+        "station":station,
+        "date":date,
+        "temperature":temperature
+    }
+if __name__ == "__main__":
+    app.run(debug=True,port=5001)
