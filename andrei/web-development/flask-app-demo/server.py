f6fb772f82be154eb2746ed0317b27b8d09fb2d8
Nithin Samuel <nithin.thewebdev@gmail.com>
2024-03-03 15:17:31 +0530

    added static files and templates

--- andrei/web-development/flask-app-demo/server.py
+++ andrei/web-development/flask-app-demo/server.py
@@ -1,7 +1,13 @@
-from flask import Flask
+# Run code > flask --app server run --debug
+from flask import Flask,render_template
 # __name__ specifies the name of app - print(__name__) = main
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World! I want to go to America</p>"
+    # return "<p>Hello, World! I want to go to America</p>"
+    return render_template('index.html')
+
+@app.route("/about.html")
+def about():
+    return render_template('about.html')
